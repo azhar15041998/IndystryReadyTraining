2638a6f774b325982363a6d8ccbcd4fe245aceda
azhar15041998 <azhar15041998@gmail.com>
2022-05-01 22:03:44 +0530

    Third commit

---
+++ LAB1_prog3.py
@@ -0,0 +1,5 @@
+#3. Write a program to find the square and cube of a given number.
+a = int(input('Enter the Number a : ')) #takes input as integer 
+square = a ** 2 
+cube = a ** 3
+print(f'Square of entered number is : {square} \nCube of entered number is : {cube}')
---
+++ LAB1_prog4.py
@@ -0,0 +1,4 @@
+#4. Write a program to find the square root of a given number.
+a = int(input('Enter the Number a ')) #takes input as integer 
+Square_Root = a ** (1/2)
+print(f'The {Square_Root} of entered number is : ')
