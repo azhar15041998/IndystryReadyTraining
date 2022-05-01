a65eaadd7348b464ac279158498c8a8937bf6dc8
azhar15041998 <azhar15041998@gmail.com>
2022-05-01 15:03:45 +0530

    second commit

---
+++ LAB1_prog1.py
@@ -0,0 +1,7 @@
+#1. Write a program to add and subtract two numbers.
+a = int(input('Enter Number a '))
+b = int(input('Enter Number b '))
+addition = a + b          #addition formula
+substractin = a - b       #subtaction formula
+print(addition)           #prints  addition
+print(substractin)        #prints   subtaction
