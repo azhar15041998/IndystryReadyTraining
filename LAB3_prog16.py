d385ca1d58839c84e0014698a3f4920d17ff970a
azhar15041998 <azhar15041998@gmail.com>
2022-05-04 21:09:53 +0530

    fourth commit

--- LAB3_prog16.py
+++ LAB3_prog16.py
@@ -7,6 +7,6 @@ y1=int(input("enter y1 : "))
 y2=int(input("enter y2 : "))
 
 #calcualtes distance between two point 
-result= ((((x2 - x1 )**2) + ((y2-y1)**2) )**0.5) 
+result= ((((x2 - x1 ) ** 2) + ((y2-y1) **2 ) ) **0.5) 
 
 print("distance between",(x1,x2),"and",(y1,y2),"is : ",result)
