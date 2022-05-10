739a0f0172c141824da16da58fe1276a4d3795f6
azhar15041998 <azhar15041998@gmail.com>
2022-05-10 17:03:42 +0530

    lower case to uppercase using upper()

---
+++ LAB9_prog38.py
@@ -0,0 +1,6 @@
+#38. Write a program to print the uppercase letter of a given lowercase.
+
+ch = input('Enter a letters in lowercase :')
+print(ch.upper())  #upper() will print lowercase to upper viceversa
+
+ 
