ad76ac0d0dbf984dc9a5dda89153ff3d2845afc6
azhar15041998 <azhar15041998@gmail.com>
2022-06-07 18:27:25 +0530

    'new commits'

--- 93.py
+++ 93.py
@@ -11,4 +11,4 @@ for i in range(6,0,-1): #range starts from 1 to 5 excludes 6
     for j in range(5,i-1,-1): #j value decreased and prints j value prints in next line
         print(j,end='')
         
-    print()  #it is same as \n
+print() #it is same as \n
